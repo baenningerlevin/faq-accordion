c310a192788b0e732d5c194a5883ddda585d9d8f
Levin Bänninger <l.baenninger@icloud.com>
2024-01-17 22:31:16 +0100

    made smooth animation

--- script.js
+++ script.js
@@ -3,9 +3,24 @@
 const questionEls = document.querySelectorAll('.question');
 
 questionEls.forEach((questionEl) => {
-  questionEl.addEventListener('click', () => {
-    questionEl.nextElementSibling.classList.toggle('hidden');
-    questionEl.children[2].classList.toggle('hidden');
-    questionEl.children[1].classList.toggle('hidden');
+  questionEl.addEventListener('click', function () {
+    // If the clicked question is already showing, hide it
+    if (this.nextElementSibling.classList.contains('show')) {
+      this.nextElementSibling.classList.remove('show');
+      this.children[2].classList.add('hidden'); // show first icon
+      this.children[1].classList.remove('hidden'); // hide second icon
+    } else {
+      // Hide all other answers and adjust icons
+      questionEls.forEach((questionEl) => {
+        questionEl.nextElementSibling.classList.remove('show');
+        questionEl.children[2].classList.add('hidden'); // show first icon
+        questionEl.children[1].classList.remove('hidden'); // hide second icon
+      });
+
+      // Show the clicked question's answer and adjust icons
+      this.nextElementSibling.classList.add('show');
+      this.children[2].classList.remove('hidden'); // hide first icon
+      this.children[1].classList.add('hidden'); // show second icon
+    }
   });
 });
